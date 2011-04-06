db1e6a1ff14e78a7ef686d4935a527072e8fd303
xavier <xavier@ac11df5e-a788-c23b-a8e1-c758c323924d>
2011-04-06 16:42:32 +0000

    profx
    git-svn-id: https://svn.ucolick.org/xsvn/trunk/radtrans@5841 ac11df5e-a788-c23b-a8e1-c758c323924d

---
+++ Winds/papers/LBG_Lya/Analysis/Shared_Codes/miki_voigt.hh
@@ -0,0 +1,32 @@
+#include "locate_array.hh"
+#include "physical_constants.hh"
+#include "cdf.hh"
+#include <gsl/gsl_integration.h>
+
+
+class VOIGT
+{
+  double *profile;
+  double a_param, voigt_u;
+  LOCATE_ARRAY x;
+  int n_x;
+  CDF *emit;
+  static double CallIntegrand(double,void*); // Michele code only
+  double Integrand(double,void*); // Michele code only
+
+public:
+  
+  void New(int n, double,  double);
+  void Compute_Profile();
+  void Print();
+  double Profile(double);
+  double Scatter_Velocity(double);
+
+};
+
+struct CCallbackHolder
+{
+  VOIGT* cls;
+  void* data;
+};
+
